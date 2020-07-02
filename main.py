247e50b12732b98e7f8e734a904a7b25931a991c
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-02 14:42:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from itertools import chain
 from pywikibot import pagegenerators
 SITE = pywikibot.Site()
 
+def uploader(filename, link=True):
+    """User that uploaded the file."""
+    history = (pywikibot.Page(SITE, filename)).revisions(reverse=True, total=1)
+    for info in history:
+        username = (info.user)
+    if not history:
+        return "Unknown"
+    if link:
+        return "[[User:%s|%s]]" % (username, username)
+    return username
 
 def list_maker():
+
     category1 = pywikibot.Category(SITE,'License review needed')
     gen1 = pagegenerators.CategorizedPageGenerator(category1)
     category2 = pywikibot.Category(SITE,'License review needed (video)')
@@ -12,8 +23,23 @@ def list_maker():
     category3 = pywikibot.Category(SITE,'License review needed (audio)')
     gen3 = pagegenerators.CategorizedPageGenerator(category3)
     gen = chain(gen1, gen2, gen3)
+
+    uploader_and_uploads = {}
+
     for page in gen:
+
         file_name = page.title()
+        if file_name.startswith("File:"):
+
+            Uploader = uploader(file_name, link=False)
+            
+            if uploader_and_uploads.get(Uploader):
+                files_list = uploader_and_uploads.get(Uploader)
+                files_list.append(file_name)
+                uploader_and_uploads[Uploader] = files_list
+            else:
+                uploader_and_uploads[Uploader] = [file_name]
+            
         
 
 def main():
