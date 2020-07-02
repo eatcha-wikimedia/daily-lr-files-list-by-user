f4dcaea5629cafcb648f0b4fde8c83dd689df255
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-02 14:30:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pywikibot
+from itertools import chain
+from pywikibot import pagegenerators
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__name__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
