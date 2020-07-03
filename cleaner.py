4b04950f1fbddbc80abe79f3e27bad3c117764af
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-03 14:39:45 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -9,19 +9,19 @@ def main(*args):
         SITE.login()
     gen = pagegenerators.CategorizedPageGenerator(pywikibot.Category(SITE,'Files requiring license review sorted by user name'))
     for page in gen:
-    namefile = page.title()
-    if namefile.startswith( 'User:EatchaBot/' ):
-        print(namefile)
-        file_page = pywikibot.Page(SITE, namefile)
-        old_text = file_page.get(get_redirect=True, force=True)
-        new_text = ""
-        EditSummary = "flushed old list, will generate new list every week."
-        if new_text == old_text:
-            continue
-        try:
-            page.put(new_text, summary=EditSummary, watch=True, minor=False)
-        except:
-            pass
+        name = page.title()
+        if name.startswith( 'User:EatchaBot/' ):
+            print(name)
+            file_page = pywikibot.Page(SITE, name)
+            old_text = file_page.get(get_redirect=True, force=True)
+            new_text = ""
+            EditSummary = "flushed old list, will generate new list every week."
+            if new_text == old_text:
+                continue
+            try:
+                page.put(new_text, summary=EditSummary, watch=True, minor=False)
+            except:
+                pass
 
 if __name__ == "__main__":
   try:
