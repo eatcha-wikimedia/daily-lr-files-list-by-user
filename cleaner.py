b65144edf77839e4781793697b0546cc86b3d0ff
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-03 14:47:49 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,22 @@
 import pywikibot
 from pywikibot import pagegenerators
+import concurrent.futures
+
+def worker():
+    name = page.title()
+    if name.startswith( 'User:EatchaBot/' ):
+        print(name)
+        file_page = pywikibot.Page(SITE, name)
+        old_text = file_page.get(get_redirect=True, force=True)
+        new_text = ""
+        EditSummary = "flushed old list, will generate new list every week."
+        if new_text == old_text:
+            continue
+        try:
+            page.put(new_text, summary=EditSummary, watch=True, minor=False)
+        except:
+            pass
+
 
 def main(*args):
     global SITE
@@ -8,20 +25,8 @@ def main(*args):
     if not SITE.logged_in():
         SITE.login()
     gen = pagegenerators.CategorizedPageGenerator(pywikibot.Category(SITE,'Files requiring license review sorted by user name'))
-    for page in gen:
-        name = page.title()
-        if name.startswith( 'User:EatchaBot/' ):
-            print(name)
-            file_page = pywikibot.Page(SITE, name)
-            old_text = file_page.get(get_redirect=True, force=True)
-            new_text = ""
-            EditSummary = "flushed old list, will generate new list every week."
-            if new_text == old_text:
-                continue
-            try:
-                page.put(new_text, summary=EditSummary, watch=True, minor=False)
-            except:
-                pass
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, gen)
 
 if __name__ == "__main__":
   try:
