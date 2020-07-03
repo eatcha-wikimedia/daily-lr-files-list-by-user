4ba3cc3061f326df219cd97b7d27a0c18cd030ff
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-03 14:36:46 +0530

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,29 +1,27 @@
 import pywikibot
 from pywikibot import pagegenerators
 
-SITE = pywikibot.Site()
-
-if not SITE.logged_in():
-  SITE.login()
-
-
-category1 = pywikibot.Category(SITE,'Files requiring license review sorted by user name')
-gen1 = pagegenerators.CategorizedPageGenerator(category1)
-
-def commit(old_text, new_text, page, comment):
-  if new_text == old_text:return
-  page.put(new_text, summary=comment, watch=False, minor=False)
-
-def main():
-  for page in gen1:
+def main(*args):
+    global SITE
+    args = pywikibot.handle_args(*args)
+    SITE = pywikibot.Site()
+    if not SITE.logged_in():
+        SITE.login()
+    gen = pagegenerators.CategorizedPageGenerator(pywikibot.Category(SITE,'Files requiring license review sorted by user name'))
+    for page in gen:
     namefile = page.title()
     if namefile.startswith( 'User:EatchaBot/' ):
-      print(namefile)
-      file_page = pywikibot.Page(SITE, namefile)
-      old_text = file_page.get(get_redirect=True, force=True)
-      new_text = ""
-      EditSummary = "flushed old list, will generate new list every week."
-      commit(old_text, new_text, file_page, "{0}".format(EditSummary))
+        print(namefile)
+        file_page = pywikibot.Page(SITE, namefile)
+        old_text = file_page.get(get_redirect=True, force=True)
+        new_text = ""
+        EditSummary = "flushed old list, will generate new list every week."
+        if new_text == old_text:
+            continue
+        try:
+            page.put(new_text, summary=EditSummary, watch=True, minor=False)
+        except:
+            pass
 
 if __name__ == "__main__":
   try:
