c694147cccf5fb3c0c942e3bb11268b9a6c6f1ee
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-04 20:30:03 +0530

    Update sorted_list.py

--- sorted_list.py
+++ sorted_list.py
@@ -20,10 +20,30 @@ def main(*args):
             uploader_and_uploads[name] = [number_of_files]
 
     sorted_num_name_dict = sorted(num_name_dict.items(), key=operator.itemgetter(1))
+    
+    init_text = """{| class="wikitable sortable"\n|-\n
+! style="background: #000000; color:   #ffffff ;" | '''Rankings ↕️'''
+! style="background: #000000; color:   #ffffff ;" | '''Username ↕️'''
+! style="background: #000000; color:   #ffffff ;" | '''Number of files requiring LR ↕️'''
+! style="background: #000000; color:   #ffffff ;" | '''Link to gallery of all files requiring LR ↕️ '''
+|- style="background:  #ffffff; color:  #000000  ;" """
+    
+    row_text = ""
+    
+    serial_no = 0
     for x in sorted_num_name_dict:
+        serial_no += 1
         name = x[0]
         count = x[1]
+        _row = """\n| %d\n| %s\n| %d\n| link to files\n|- style="background:  #ffffff; color:  #000000  ;" """ % (
+            serial_no,
+            name,
+            
+            
+            )
+        row_text = row_text + 
         
+
 
     
             
