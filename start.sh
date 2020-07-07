d8d0c014906c3edd417150216d7228ad12a483a8
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-07 22:49:04 +0530

    User configuration path dir

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd ~/daily-lr-files-list-by-user
 git pull --all && git submodule update
 cd -
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/cleaner.py -pt:0
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery_audio_video.py -pt:0
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery_image.py -pt:0
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/sorted_list.py -pt:0
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/cleaner.py -pt:0 -dir:~/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery_audio_video.py -pt:0 -dir:~/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery_image.py -pt:0 -dir:~/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/sorted_list.py -pt:0 -dir:~/daily-lr-files-list-by-user
