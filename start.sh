dd6bcfe1fbd4d82144036cc822f0077a620c11ac
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-08 09:34:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 cd ~/daily-lr-files-list-by-user
 git pull --all && git submodule update
 cd ~
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/cleaner.py -pt:0 -dir:/data/project/eatchabot/daily-lr-files-list-by-user
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery.py -pt:0 -dir:/data/project/eatchabot/daily-lr-files-list-by-user
-~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/sorted_list.py -pt:0 -dir:/data/project/eatchabot/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/cleaner.py -pt:0 -dir:~/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/gallery.py -pt:0 -dir:~/daily-lr-files-list-by-user
+~/lr-list-env/bin/python3 ~/daily-lr-files-list-by-user/sorted_list.py -pt:0 -dir:~/daily-lr-files-list-by-user
