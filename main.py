81cbd2c82f116d6609a96b85b35418ed3f411b1a
User:Eatcha <eatcha-wikimedia@protonmail.com>
2020-07-08 10:28:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+import pywikibot
+import re
+from datetime import datetime
+from pywikibot import pagegenerators
+import concurrent.futures
+from itertools import chain
+
+# WTF am I using 2 mwapi API libs ? I love pywikibot but there's a bug in the pagegenerator 
+# The bug: try iteration over all the file in commons category "License review needed"
+# The generator doesn't exhaust , why ? IDK
+
+import mwclient
+site = mwclient.Site(('https', 'commons.wikimedia.org'))
+
+f=open("/data/project/eatchabot/passwords/mwc.py","r")
+lines=f.readlines()
+username=lines[0].strip()
+password=lines[1].strip()
+f.close()
+print(username)
+print(password)
+
+site.login(username, password)
+
+def uploader(filename, link=True):
+    """User that uploaded the file."""
+    history = (pywikibot.Page(SITE, filename)).revisions(reverse=True, total=1)
+    for info in history:
+        username = (info.user)
+    if not history:
+        return "Unknown"
+    if link:
+        return "[[User:%s|%s]]" % (username, username)
+    return username
+
+def out(text, newline=True, date=False, color=None):
+    """output some text to the consoloe / log."""
+    if color:
+        text = "\03{%s}%s\03{default}" % (color, text)
+    dstr = (
+        "%s: " % datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        if date
+        else ""
+    )
+    pywikibot.stdout("%s%s" % (dstr, text), newline=newline)
+
+def commit(old_text, new_text, page, summary):
+    """Show diff and submit text to page."""
+    out("\nAbout to make changes at : '%s'" % page.title())
+    pywikibot.showDiff(old_text, new_text)
+    page.put(new_text, summary=summary, watchArticle=True, minorEdit=False)
+
+__count = 0
+
+uploader_files_list_dict = {}
+
+def dict_maker(page):
+    file_name = page.name
+    if file_name.startswith("File:"):
+        global __count
+        __count += 1 
+        out("%d - %s" % (__count, file_name))
+
+        Uploader = uploader(file_name, link=False)
+        global uploader_files_list_dict
+        if uploader_files_list_dict.get(Uploader):
+            list_ = uploader_files_list_dict.get(Uploader)
+            list_.append(file_name)
+            uploader_files_list_dict[Uploader] = list_
+        else:
+            _list = []
+            _list.append(file_name)
+            uploader_files_list_dict[Uploader] = _list
+
+def operator(param):
+    Uploader = param[0]
+    file_list = param[1]
+    out(Uploader)
+
+    user_review_subpage_name = "User:EatchaBot/Files-requiring-license-review-gallery-uploaded-by/%s" % Uploader
+    user_review_subpage = pywikibot.Page(SITE, user_review_subpage_name)
+    try:
+        user_review_subpage_old_text = user_review_subpage.get(get_redirect=True, force=True)
+    except pywikibot.NoPage:
+        user_review_subpage_old_text = """<p style="border-top: 2px solid #000;border-bottom: 2px solid #000;background-color: #6f6e6d ;color:#ffffff" align="center">&#8594; Sorted list available at [[User:EatchaBot/Files-requiring-license-review-sorted-list|<span style="color:#ffffff">'''User:EatchaBot/Files-requiring-license-review-sorted-list'''</span>]].</p>\n<gallery showfilename=yes>\n</gallery>\n[[Category:Files requiring license review sorted by user name]]"""
+    
+    if (user_review_subpage_old_text.find('<gallery showfilename=yes>') != -1):
+        pass
+    else:
+        user_review_subpage_old_text = """<p style="border-top: 2px solid #000;border-bottom: 2px solid #000;background-color: #6f6e6d ;color:#ffffff" align="center">&#8594; Sorted list available at [[User:EatchaBot/Files-requiring-license-review-sorted-list|<span style="color:#ffffff">'''User:EatchaBot/Files-requiring-license-review-sorted-list'''</span>]].</p>\n<gallery showfilename=yes>\n</gallery>\n[[Category:Files requiring license review sorted by user name]]"""
+    
+    _count = user_review_subpage_old_text.count("File:")
+    _text = ""
+
+    for file_name in file_list:
+
+        if file_name in user_review_subpage_old_text:
+            continue
+
+        _count += 1
+        row =  "%s|%s\n" % (file_name, _count)
+        _text = _text + row
+    
+    _text = _text + "</gallery>"
+
+    new_text = user_review_subpage_old_text.replace("</gallery>", _text)
+    user_review_subpage_EditSummary = "Adding %d files" % (len(file_list))
+    
+    if user_review_subpage_old_text == new_text:
+        return
+    try:
+        commit(user_review_subpage_old_text, new_text, user_review_subpage, "{0}".format(user_review_subpage_EditSummary))
+    except Exception as e:
+        out(e)
+        return
+
+def gallery_maker():
+
+    gen1 = site.Categories['License review needed']
+    gen2 = site.Categories['License review needed (video)']
+    gen3 = site.Categories['License review needed (audio)']
+    gen_list = [gen1, gen2, gen3]
+
+    for gen in gen_list:
+        try:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+                    executor.map(dict_maker, gen)
+        except IndexError:
+            pass
+
+    global uploader_files_list_dict
+    with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+        executor.map(operator, uploader_files_list_dict.items())
+
+def cleaner_worker(page):
+    name = page.title()
+    if name.startswith( 'User:EatchaBot/' ):
+        print(name)
+        file_page = pywikibot.Page(SITE, name)
+        old_text = file_page.get(get_redirect=True, force=True)
+        if old_text.isspace():
+            return
+        EditSummary = "flushed old list, will generate new list every week."
+        try:
+            page.put("", summary=EditSummary, watch=True, minor=False)
+        except:
+            pass
+
+def main(*args):
+    global SITE
+    args = pywikibot.handle_args(*args)
+    SITE = pywikibot.Site()
+    if not SITE.logged_in():
+        SITE.login()
+
+    gen = pagegenerators.CategorizedPageGenerator(pywikibot.Category(SITE, 'Files requiring license review sorted by user name'))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(cleaner_worker, gen)
+
+    gallery_maker()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
